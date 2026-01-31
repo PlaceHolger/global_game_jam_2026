c0a55904abaa5efc6972ea048d1f20eeededd80a
Dominik Ospelt <dominiko@binaryimpact.de>
2026-01-31 21:43:15 +0100

    Fix old input dependency

--- Assets/Warmask.Mask/Runtime/MaskManager.cs
+++ Assets/Warmask.Mask/Runtime/MaskManager.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Runtime.InteropServices.ComTypes;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 
@@ -11,6 +13,9 @@ namespace Warmask.Mask
     {
         [SerializeField] Volume Mask1Volume;
         [SerializeField] Volume Mask2Volume;
+
+        [SerializeField] private InputActionReference mask1Action;
+        [SerializeField] private InputActionReference mask2Action;
 
         public void ToggleMask(Globals.eType type)
         {
@@ -28,27 +33,43 @@ namespace Warmask.Mask
 
         private void UpdateMask(Globals.eType type)
         {
+            Debug.Log($"[{nameof(MaskManager)}] Updating mask to {type}");
+            Globals.Instance.currentMask = type;
             Globals.Instance.OnMaskChanged.Invoke(type);
             //tween volume weights
-            if (Mask1Volume && Mask2Volume)
+            if (Mask1Volume)
             {
                 DOTween.To(() => Mask1Volume.weight, x => Mask1Volume.weight = x, type == Globals.eType.TypeA ? 1f : 0f,
                     0.5f);
+            }
+
+            if (Mask2Volume)
+            {
                 DOTween.To(() => Mask2Volume.weight, x => Mask2Volume.weight = x, type == Globals.eType.TypeB ? 1f : 0,
                     0.5f);
             }
         }
 
-        private void Update()
+        private void OnEnable()
         {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                ToggleMask(Globals.eType.TypeA);
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                ToggleMask(Globals.eType.TypeB);
-            }
+            if (mask1Action != null) mask1Action.action.performed += ChangeMask1;
+            if (mask2Action != null) mask2Action.action.performed += ChangeMask2;
+        }
+
+        private void OnDisable()
+        {
+            if (mask1Action != null) mask1Action.action.performed -= ChangeMask1;
+            if (mask2Action != null) mask2Action.action.performed -= ChangeMask2;
+        }
+
+        private void ChangeMask1(InputAction.CallbackContext ctx)
+        {
+            if (ctx.phase == InputActionPhase.Started) ToggleMask(Globals.eType.TypeA);
+        }
+
+        private void ChangeMask2(InputAction.CallbackContext ctx)
+        {
+            if (ctx.phase == InputActionPhase.Started) ToggleMask(Globals.eType.TypeB);
         }
     }
 }
