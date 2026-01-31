144130a64c5185c0565123850bf1bc452b08cb65
Dominik Ospelt <dominiko@binaryimpact.de>
2026-01-31 21:06:34 +0100

    Ship mask filter

---
+++ Assets/Warmask.Ship/Runtime/ShipMaskFader.cs
@@ -0,0 +1,44 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Warmask.Ship
+{
+    public class ShipMaskFader : MonoBehaviour
+    {
+        [SerializeField] private Globals.eType type;
+        
+
+        private void OnEnable()
+        {
+            Globals.Instance.OnMaskChanged.AddListener( MaskChanged );
+        }
+
+        private void OnDisable()
+        {
+            Globals.Instance.OnMaskChanged.RemoveListener( MaskChanged );
+        }
+
+        private void MaskChanged(Globals.eType newType)
+        {
+            Color c = Globals.Instance.GetTypeColor(type);
+            c.a = (newType == type) ? 1f : 0f;
+            SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer sr in spriteRenderers)
+            {
+                if (sr != null)
+                {
+                    sr.color = c;
+                }
+            }
+            TrailRenderer[] trailRenderers = GetComponentsInChildren<TrailRenderer>();
+            foreach (TrailRenderer tr in trailRenderers)
+            {
+                if (tr != null)
+                {
+                    tr.startColor = c;
+                }
+            }
+        }
+    }
+}
